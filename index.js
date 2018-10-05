f8c2ec5dfb12fca39164d3da3f8828ae9c566902
Marcy Silverman <marcysilve225@gmail.com>
2018-10-04 19:47:59 -0700

    guessing game

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ function handleSubmit(event) {
     var form = event.target;
     var elements = form.elements;
 
-    var name = elements.name.value
-    console.log('name', name); 
-    
-    var comments = elements.comments.value; 
-    console.log('comments', comments);
-    
-    var toppings = elements.toppings.value; 
-    console.log('topping', toppings);
-    
-    var grade = elements.grade.value;
-    console.log('grade', grade); 
+    var guess = elements.guess.value; 
+    console.log('guess', guess);
+
+    var result = document.getElementById('result');
+    //result.textContent= 'You made a guess of ' + guess; 
+
+    var correct = 2; 
+    if (guess == correct) {
+        result.textContent='You are correct!'; 
+    }
+    else if (guess > 2) {
+        result.textContent= 'Too high!';
+    }
+    else {
+        result.textContent= 'Too low!';
+    }
+    //if equal -> correct! 
+    //else if less than -> too low! 
 }
