b03e796818a21c327968fac4c9cc217ea4f3980b
Marcy Silverman <marcysilve225@gmail.com>
2018-10-04 20:58:54 -0700

    pizza toppings

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 function handleSubmit(event) {
     var form = event.target;
     var elements = form.elements;
+    var toppings = elements.toppings;
 
-    var guess = elements.guess.value; 
-    console.log('guess', guess);
+    var selectedToppings = ' ';
 
-    var result = document.getElementById('result');
-    //result.textContent= 'You made a guess of ' + guess; 
-
-    var correct = 2; 
-    if (guess == correct) {
-        result.textContent ='You are correct!'; 
+    for (var i = 0; i < toppings.length; i++) {
+        var topping = toppings[i];
+        if(topping.checked) {
+            selectedToppings += ' ' + topping.value;
+        }
     }
-    else if (guess > 2) {
-        result.textContent= 'Too high!';
-    }
-    else {
-       result.textContent= 'Too low!';
-    }
-    //if equal -> correct! 
-    //else if less than -> too low! 
+        console.log(selectedToppings);
 }
+
